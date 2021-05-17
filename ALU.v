80d643e58fb9e3fab3418558fb965c4025641e3b
André Arias <ariasandreccs@gmail.com>
2021-05-16 22:08:45 -0600

    Codigo ALU y test ALU

---
+++ ALU.v
@@ -0,0 +1,54 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10.05.2021 11:49:18
+// Design Name: 
+// Module Name: ALU
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ALU(
+    /* Se definen las entradas y salidas del modulo*/
+    input [31:0] rs1, rs2,
+    input [2:0] sel,
+    input clk,
+    output reg [31:0] sal
+    );
+    
+    always @(posedge clk)
+        begin
+            case(sel)
+                0:
+                    sal = rs1 + rs2;
+                1:
+                    sal = rs1 & rs2;
+                2:
+                    sal = rs1 ^ rs2;
+                3:
+                    sal = rs1 << rs2;
+                4:
+                    sal = rs1 >>> rs2;
+                5: 
+                    sal = rs1 - rs2;
+                6:
+                    sal = (rs1 + rs2) & 32'hFFFE;
+                7:
+                    sal = 32'h0000;
+                default:
+                    sal = 32'h0000;
+            endcase
+        end
+endmodule
---
+++ SIM_ALU.v
@@ -0,0 +1,58 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 13.05.2021 18:37:48
+// Design Name: 
+// Module Name: SIM_ALU
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module SIM_ALU(
+    );
+    
+    reg [31:0] rs1, rs2;
+    reg clk;
+    reg [2:0] sel;
+    
+    wire [31:0] sal;
+    
+    ALU uut(
+        .rs1(rs1),
+        .rs2(rs2),
+        .sel(sel),
+        .clk(clk),
+        .sal(sal)
+    );
+    
+    initial begin
+        rs1 = 226;
+        rs2 = 7;
+        sel = 0;
+        clk = 0;
+    end
+    
+    always
+        #5 clk = ~clk;
+        
+    always@(posedge clk)
+        begin
+            sel = sel + 1;
+        end
+    
+    initial
+    #85 $finish;
+    
+endmodule
